373d9417125773ca36c18706c1ac9fafd1147f31
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-09 12:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	const methodNames = Object.getOwnPropertyNames(Math);
+
+	const result = methodNames.join(", ");
+	return result;
 }
 
-alert(allMethods());
+alert(allMethods()); 
